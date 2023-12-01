a4fe664e94fa0374b00e06a903275417b73c54a3
Amirianfar <amirianfar@gmail.com>
2023-12-01 19:38:14 +0330

    First

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+namespace HelloWorld
+{
+
+    class Program 
+    {
+      static void Main (String[] args) 
+      {
+        Console.WriteLine("Hello, World! ");
+      }
+    }
+}
